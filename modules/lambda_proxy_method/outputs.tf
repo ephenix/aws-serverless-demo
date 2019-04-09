d31c43fbda6d62c5b1e4c78b951d68d16e87cab7
Eric Phenix <github@ericphenix.com>
2019-04-08 23:15:38 -0600

    add depends_on to module output to prevent race condition

--- modules/lambda_proxy_method/outputs.tf
+++ modules/lambda_proxy_method/outputs.tf
@@ -1,3 +1,8 @@
 output "rest_api_id" {
     value = "${var.rest_api_id}"
+    depends_on = [
+        "aws_api_gateway_integration.integration",
+        "aws_api_gateway_integration_response.integration_response",
+        "aws_api_gateway_method_response.method_response"
+    ]
 }
